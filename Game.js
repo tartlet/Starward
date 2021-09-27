c09b0414e8e57cc07950b597f3fb0d1fc70a9d71
Sinbad <43188352+Anima11@users.noreply.github.com>
2021-09-27 14:38:57 -0500

    Attacks, Damage & Enemies
    Added damage inflicted by player
    Added player attacks with spacebar
    added enemies spawning
    Enemies can be killed by player
    Added tutorial text
    Crude placeholder assets used

--- Game.js
+++ Game.js
@@ -2,14 +2,21 @@ var Starward = Starward || {};
 
 Starward.Game = function () {};
 var total = 0;
+var numEnemies = 3;
+var chestSpawned = false;
 
 Starward.Game.prototype = {
     
     create: function () {
+        
+        
+        
         timer = this.time.create();
         map = this.add.tilemap('tilemap1');
         tileset = map.addTilesetImage('tset1', 'tileset1');
         this.layer = map.createLayer(0,'toplayer', tileset);
+        
+        
         player = this.add.sprite(0, 128, 'player');
         player.enableBody = true;
         this.physics.arcade.enable(player);
@@ -17,13 +24,32 @@ Starward.Game.prototype = {
         this.countdown = this.add.text('$: ' + this.money, 10);
         player.animations.add('idle', [1, 2, 3, 4, 5, 6], 10, true);
         player.animations.add('walk', [7, 8, 9, 10], 100, true);
-
-        chest = this.add.sprite(128, 0, 'chest');
-        chest.enableBody = true;
-        this.physics.arcade.enable(chest);
+        
+        // Move this chunk later to add the chest after the enemies are slain
+        
+        
+        // Add the enemy animations below
+        enemy1 = this.add.sprite(128, 128, 'basicEnemy');
+        enemy2 = this.add.sprite(64, 64, 'basicEnemy');
+        enemy3 = this.add.sprite(64, 0, 'basicEnemy');
+        
+        // Enable the bodies for enemies below
+        enemy1.enableBody = true;
+        enemy2.enableBody = true;
+        enemy3.enableBody = true;
+        
+        // Set Enemy HP
+        enemy1.health = 10;
+        enemy2.health = 10;
+        enemy3.health = 10;
+        
+        
         cursors = this.input.keyboard.createCursorKeys();
+        
+        
         money = 0;
         this.scoreText = this.add.text(0, 0, '$:0', {font:'15px Comic Sans MS', fill:'#fff'});
+        this.attackText = this.add.text(0, 140, 'Spacebar to attack!', {font: '12px Comic Sans MS', fill: '#fff'});
     },
 
     movePlayer: function (x, y) {
@@ -47,10 +73,56 @@ Starward.Game.prototype = {
     },
 
     update: function () {
-        this.physics.arcade.overlap(chest, player, this.collideMoney, null, this);
+        
+        if (chestSpawned == true){
+            this.physics.arcade.overlap(chest, player, this.collideMoney, null, this);
+        }
+        
+        
+        // While nothing is being pressed, make player stand still
         player.body.velocity.x = 0;
         player.body.velocity.y = 0;
         player.animations.play('idle');
+        
+        // Check for dead enemies
+        if (enemy1.health <= 0){    // Change this code later, could work by putting them in arrray
+            enemy1.destroy();
+            numEnemies -= 1;
+            enemy1.health = 10;
+        }
+        else if (enemy2.health <= 0){
+            enemy2.destroy();
+            numEnemies -= 1;
+            enemy2.health = 10;
+        }
+        else if (enemy3.health <= 0){
+            enemy3.destroy();
+            numEnemies -= 1;
+            enemy3.health = 10;
+        }
+        
+        // Player attack
+        if (this.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)){
+            
+            attack = this.game.add.sprite(player.x + 32, player.y, 'playerAttack'); // Also store these in an array later
+            
+            if (attack.x == enemy1.x && attack.y == enemy1.y && enemy1.health > 0){     // Code heavily subject to change
+                // decrease enemy health
+                enemy1.health -= 1;
+                
+            }
+            else if (attack.x == enemy2.x && attack.y == enemy2.y && enemy2.health > 0){
+                enemy2.health -=1;
+                
+            }
+            else if (attack.x == enemy3.x && attack.y == enemy3.y && enemy3.health > 0){
+                enemy3.health -= 1;
+                
+            }
+            
+        }
+        
+        // Player movement
         if (cursors.left.isDown) {
             //  Move to the left
             this.movePlayer(-1, 0);
@@ -75,6 +147,15 @@ Starward.Game.prototype = {
         }
         else {
             player.animations.play('idle');
+        }
+        
+        // When all enemies are defeated, spawn the chest to proceed to the shop
+        if (numEnemies == 0 && chestSpawned == false){
+            chest = this.add.sprite(128, 0, 'chest');
+            chest.enableBody = true;
+            this.physics.arcade.enable(chest);
+            chestSpawned = true;
+            this.proceedText = this.add.text(0, 64, 'Collect chest to proceed!', {font: '12px Comic Sans MS', fill: '#fff'});
         }
 
         if(this.gameover == true){
--- Preload.js
+++ Preload.js
@@ -6,16 +6,30 @@ Starward.Preload = function(game) {
 
 Starward.Preload.prototype = {
     preload: function() {
-    this.preloadBar = this.add.sprite(this.game.world.centerX, this.game.world.centerY + 128, 'preloadBar');
-    this.preloadBar.anchor.setTo(0.5);
-    this.load.setPreloadSprite(this.preloadBar);
+        this.preloadBar = this.add.sprite(this.game.world.centerX, this.game.world.centerY + 128, 'preloadBar');
+        this.preloadBar.anchor.setTo(0.5);
+        this.load.setPreloadSprite(this.preloadBar);
 
-    this.load.tilemap('tilemap1', 'assets/level1.csv', null, Phaser.Tilemap.CSV);
-    this.load.image('tileset1', 'assets/TileMap.png');
-    this.load.image('level1bg', 'assets/sky.png');
-    this.load.spritesheet('player', 'assets/forgotten.png', 32, 32);
-    this.load.spritesheet('chest', 'assets/chest.png', 32, 32);
-
+        this.load.tilemap('tilemap1', 'assets/level1.csv', null, Phaser.Tilemap.CSV);
+        this.load.image('tileset1', 'assets/TileMap.png');
+        this.load.image('level1bg', 'assets/sky.png');
+        this.load.spritesheet('player', 'assets/forgotten.png', 32, 32);
+        
+        
+        
+        // Load the player's attacking spritesheet below
+        this.load.image('playerAttack', 'assets/PlayerBasicAttack.png', 32, 32);
+        
+        this.load.spritesheet('chest', 'assets/chest.png', 32, 32);
+    
+        // Load the enemy sprites below
+        this.load.image('basicEnemy', 'assets/BasicEnemySprite.png', 32, 32);
+        
+        // Load the enemy danger squares below
+        this.load.image('dangerSquare', 'assets/DangerSquare.png', 32, 32);
+        
+        
+        
     },
 
     create: function() {

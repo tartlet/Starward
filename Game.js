dd69e417f0498b4aa1cbcdb25a77d1a100b680e5
tartlet <jingsi.zhou66@gmail.com>
2021-10-06 11:53:52 -0500

    new animations, VERY buggy

--- Game.js
+++ Game.js
@@ -8,22 +8,28 @@ var chestSpawned = false;
 Starward.Game.prototype = {
     
     create: function () {
-        
-        
-        
-        timer = this.time.create();
+        game.physics.startSystem(Phaser.Physics.Arcade);
         map = this.add.tilemap('tilemap1');
-        tileset = map.addTilesetImage('tset1', 'tileset1');
-        this.layer = map.createLayer(0,'toplayer', tileset);
+        tileset = map.addTilesetImage('starward', 'tileset1', 64, 64);
+        this.layer1 = map.createLayer('bg');
+        this.layer2 = map.createLayer('ground');
+        map.setCollision(13, true, this.layer);
+        this.layer2.resizeWorld();
         
         
-        player = this.add.sprite(0, 128, 'player');
+        player = this.add.sprite(192, 512, 'player');
         player.enableBody = true;
         this.physics.arcade.enable(player);
         player.body.collideWorldBounds = true;
         this.countdown = this.add.text('$: ' + this.money, 10);
-        player.animations.add('idle', [1, 2, 3, 4, 5, 6], 10, true);
-        player.animations.add('walk', [7, 8, 9, 10], 100, true);
+        player.animations.add('idle_f', [1, 2, 3, 4, 5, 6], 10, true);
+        player.animations.add('walk_f', [7, 8, 9, 10], 100, true);
+        player.animations.add('idle_r', [11, 12, 13, 14, 15, 16], 10, true);
+        player.animations.add('walk_r', [17, 18, 19, 20], 100, true);
+        player.animations.add('idle_l', [27, 28, 29, 30, 31, 32], 10, true);
+        player.animations.add('walk_l', [33, 34, 35, 36], 100, true);
+        player.animations.add('idle_b', [37, 38, 39, 40, 41, 42], 10, true);
+        player.animations.add('walk_b', [43, 44, 45, 46], 100, true);
         
         // Move this chunk later to add the chest after the enemies are slain
         
@@ -45,6 +51,7 @@ Starward.Game.prototype = {
         
         
         cursors = this.input.keyboard.createCursorKeys();
+        game.input.keyboard.addKey.Q;
         
         
         money = 0;
@@ -57,7 +64,7 @@ Starward.Game.prototype = {
             return;
         }
         player.isMoving = true;
-        this.add.tween(player).to({x: player.x + x * 32, y: player.y + y * 32 }, 
+        this.add.tween(player).to({x: player.x + x * 64, y: player.y + y * 64}, 
             150, Phaser.Easing.Quadratic.InOut, true).onComplete.add(function() {
             player.isMoving = false;}, this);
     },
@@ -73,7 +80,7 @@ Starward.Game.prototype = {
     },
 
     update: function () {
-        
+        game.physics.arcade.collide('player', map);
         if (chestSpawned == true){
             this.physics.arcade.overlap(chest, player, this.collideMoney, null, this);
         }
@@ -127,25 +134,26 @@ Starward.Game.prototype = {
             //  Move to the left
             this.movePlayer(-1, 0);
             //player.body.velocity.x = -200;
-            player.animations.play('walk');
+            player.animations.play('walk_l');
         }
         else if (cursors.right.isDown) {
             //  Move to the right
             this.movePlayer(1, 0);
             //player.body.velocity.x = 200;
-            player.animations.play('walk');
+            player.animations.play('walk_r');
         }
         else if (cursors.up.isDown) {
             this.movePlayer(0, -1);
             //player.body.velocity.y = -200;
-            player.animations.play('walk');
+            player.animations.play('walk_b');
         }
         else if (cursors.down.isDown) {
             this.movePlayer(0, 1);
             //player.body.velocity.y = 200;
-            player.animations.play('walk');
+            player.animations.play('walk_f');
         }
         else {
+            player.animations.stop();
             player.animations.play('idle');
         }
         
--- Preload.js
+++ Preload.js
@@ -10,10 +10,9 @@ Starward.Preload.prototype = {
         this.preloadBar.anchor.setTo(0.5);
         this.load.setPreloadSprite(this.preloadBar);
 
-        this.load.tilemap('tilemap1', 'assets/level1.csv', null, Phaser.Tilemap.CSV);
-        this.load.image('tileset1', 'assets/TileMap.png');
-        this.load.image('level1bg', 'assets/sky.png');
-        this.load.spritesheet('player', 'assets/forgotten.png', 32, 32);
+        this.load.tilemap('tilemap1', 'assets/level1.json', null, Phaser.Tilemap.TILED_JSON);
+        this.load.image('tileset1', 'assets/tilemap_64px.png');
+        this.load.spritesheet('player', 'assets/forgottenv2_64px.png', 64, 64);
         
         
         

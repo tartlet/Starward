36d103dad50dc9a1cd4ec874b326cf8350c2b4c1
sanchezsammy45 <66335083+sanchezsammy45@users.noreply.github.com>
2021-11-05 14:43:57 -0500

    correction to previous push

--- tutorial.js
+++ tutorial.js
@@ -116,24 +116,36 @@ Starward.Game.prototype = {
                     facingDirection = enemies[i].attackDirection;
                     
                     // add the projectile to be shot from the square in front of the enemy
-                    // array = [x,y,velocity]
-                    const coordinates = [[0,64, 254], [-64,0, -256], [0,-64, -265], [64,0, 256]] 
-                    currentCoordinates = coordinates[facingDirection-1]
-
-                    turretProjectile = this.add.sprite(enemies[i].x + currentCoordinates[0], enemies[i].y + currentCoordinates[1], 'turretBul');
-                    turretProjectile.enableBody = true;
-                    this.physics.arcade.enable(turretProjectile);
-
-                    if (facingDirection == 1 || facingDirection == 3){
-                        turretProjectile.body.velocity.y = currentCoordinates[2];    
-                    }
-                    else{
-                        turretProjectile.body.velocity.x = currentCoordinates[2];
+                    if (facingDirection == 1){
+                        turretProjectile = this.add.sprite(enemies[i].x, enemies[i].y + 64, 'turretBul');
+                        turretProjectile.enableBody = true;
+                        this.physics.arcade.enable(turretProjectile);
+                        turretProjectile.animations.add('spinning', [1, 2, 3, 4], 4, true);
+                        turretProjectile.animations.play('spinning');
+                        turretProjectile.body.velocity.y = 256;
+                    } else if (facingDirection == 2){
+                        turretProjectile = this.add.sprite(enemies[i].x - 64, enemies[i].y, 'turretBul');
+                        turretProjectile.enableBody = true;
+                        this.physics.arcade.enable(turretProjectile);
+                        turretProjectile.animations.add('spinning', [1, 2, 3, 4], 4, true);
+                        turretProjectile.animations.play('spinning');
+                        turretProjectile.body.velocity.x = -256;
+                    } else if (facingDirection == 3){
+                        turretProjectile = this.add.sprite(enemies[i].x, enemies[i].y - 64, 'turretBul');
+                        turretProjectile.enableBody = true;
+                        this.physics.arcade.enable(turretProjectile);
+                        turretProjectile.animations.add('spinning', [1, 2, 3, 4], 4, true);
+                        turretProjectile.animations.play('spinning');
+                        turretProjectile.body.velocity.y = -256;
+                    } else if (facingDirection == 4){
+                        turretProjectile = this.add.sprite(enemies[i].x + 64, enemies[i].y, 'turretBul');
+                        turretProjectile.enableBody = true;
+                        this.physics.arcade.enable(turretProjectile);
+                        turretProjectile.animations.add('spinning', [1, 2, 3, 4], 4, true);
+                        turretProjectile.animations.play('spinning');
+                        turretProjectile.body.velocity.x = 256;
                     }
                     
-                    turretProjectile.animations.add('spinning', [1, 2, 3, 4], 4, true);
-                    turretProjectile.animations.play('spinning');
-
                     projectiles.push(turretProjectile);
                 }
                 
@@ -188,137 +200,78 @@ Starward.Game.prototype = {
     
     // randomized enemy spawner that can/will vary from level to level
     enemySpawner: function(enemies, totalEnemies) {
-
-        // every possible space for a turret
-        var spaces = [[2,3],[2,4],[2,5],[2,6],[2,7],[2,8],[9,3],[9,4],[9,5],[9,6],[9,7],[9,8],[3,2],[4,2],[5,2],[6,2],[7,2],[8,2],[3,9],[4,9],[5,9],[6,9],[7,9],[8,9]]
+        var randLocation;
+        var previousRandLocation = -1;
         var direction;
-       
+        var row;
+        var column;
+        var otherPoint;
+        
+        // loop until totalEnemies is reached
         for ( i = 0; i < totalEnemies; i++) {
+            // need to make a random integer from 0-23 that representes the clockwise order of the spaces surrounding the board
+            randLocation = Math.floor(Math.random() * 24);
+            // then whatever that location is i can divide that by 6 to find in which direction the enemy should be placed
+            // if the result is 0-1 then we are north, 1-2 is east, 2-3 is south, 3-4 is west
+            if (randLocation / 6 <= 1) {
+                direction = 1;
+            } else if (randLocation / 6 > 1 && randLocation / 6 <= 2) {
+                direction = 2;
+            } else if (randLocation / 6 > 2 && randLocation / 6 <= 3) {
+                direction = 3;
+            } else if (randLocation / 6 > 3 && randLocation / 6 <= 4) {
+                direction = 4;
+            }
+            // reset the values so that it doesn't mess up the loops
+            row = 0;
+            column = 0;
+            // direction 1 is the 3rd row and has columns 4-9 (north)
+            // direction 2 is the 10th column and has rows 4-9 (east)
+            // direction 3 is the 10th row and has columns 4-9 (south)
+            // direction 4 is the 3rd column and has rows 4-9 (west)
+            // need to subtract 1 from the row and column though because it technically starts at 0 not 1
             
-            random = Math.floor(Math.random() * spaces.length);
-            selection = spaces[random];
-           
-            // set direction, 1 is north, 2 east, 3 south, 4 west
-            if (selection[1] == 2){
-                direction = 1
-            } 
-            else if(selection[0] == 9){
-                direction = 2
-            } 
-            else if(selection[1] == 9){
-                direction = 3
-            } 
-            else{
-                direction = 4
+            // now write if-statements for each direction and assign the starting row or column based on that
+            if (direction == 1) {
+                row = 2;
+            } else if (direction == 2) {
+                column = 9;
+            } else if (direction == 3) {
+                row = 9;
+            } else if (direction == 4) {
+                column = 2;
+            }
+            // now i need to get which exact second coordinate point their at by using modulus
+            otherPoint = randLocation % 6 + 3;
+            // with this, i get either 0, 1, 2, 3, 4, or 5 then add 3 to not have them spawn in corners
+            
+            if (row == 2 || row == 9) {
+                column = otherPoint;
+            } else {
+                row = otherPoint;
             }
             
-            // spawn the enemy
-            enemy = this.add.sprite( 64 * selection[0], 64 * selection[1], 'turret' + direction.toString());
-            enemy.enableBody = true;
-            
-            // set their stats
-            enemy.health = 10;
-            enemy.fireRate = 3; // seconds
-            enemy.damage = 1;   // takes away 1 player heart
-            enemy.attackDirection = direction;
-            // . . .
-            
-            
-            // then add them to the current list of enemies
-            enemies.push(enemy);
-
-            // delete the selection from spaces array to avoid turret in same space
-            var deleted = spaces.splice(random,1);
-
-
-        
-        //                                                       //
-        //                                                       //
-        //  PREVIOUS CODE FOR TURRET PLACEMENT COMMENTED BELOW   //
-        //                                                       //
-        //                                                       //
-
-
-        // var randLocation;
-        // var previousRandLocation = -1;
-        // var direction;
-        // var row;
-        // var column;
-        // var otherPoint;
-
-        // // loop until totalEnemies is reached
-        // for ( i = 0; i < totalEnemies; i++) {
-        //     // need to make a random integer from 0-23 that representes the clockwise order of the spaces surrounding the board
-        //     randLocation = Math.floor(Math.random() * 24);
-        //     // then whatever that location is i can divide that by 6 to find in which direction the enemy should be placed
-        //     // if the result is 0-1 then we are north, 1-2 is east, 2-3 is south, 3-4 is west
-        //     if (randLocation / 6 <= 1) {
-        //         direction = 1;
-        //     } else if (randLocation / 6 > 1 && randLocation / 6 <= 2) {
-        //         direction = 2;
-        //     } else if (randLocation / 6 > 2 && randLocation / 6 <= 3) {
-        //         direction = 3;
-        //     } else if (randLocation / 6 > 3 && randLocation / 6 <= 4) {
-        //         direction = 4;
-        //     }
-        //     // reset the values so that it doesn't mess up the loops
-        //     row = 0;
-        //     column = 0;
-        //     // direction 1 is the 3rd row and has columns 4-9 (north)
-        //     // direction 2 is the 10th column and has rows 4-9 (east)
-        //     // direction 3 is the 10th row and has columns 4-9 (south)
-        //     // direction 4 is the 3rd column and has rows 4-9 (west)
-        //     // need to subtract 1 from the row and column though because it technically starts at 0 not 1
-            
-        //     // now write if-statements for each direction and assign the starting row or column based on that
-        //     if (direction == 1) {
-        //         row = 2;
-        //     } else if (direction == 2) {
-        //         column = 9;
-        //     } else if (direction == 3) {
-        //         row = 9;
-        //     } else if (direction == 4) {
-        //         column = 2;
-        //     }
-        //     // now i need to get which exact second coordinate point their at by using modulus
-        //     otherPoint = randLocation % 6 + 3;
-        //     // with this, i get either 0, 1, 2, 3, 4, or 5 then add 3 to not have them spawn in corners
-            
-        //     if (row == 2 || row == 9) {
-        //         column = otherPoint;
-        //     } else {
-        //         row = otherPoint;
-        //     }
-            
-        //     // I now have coordinates for where I want the enemy to be placed
-        //     // next is to check whether there exists an enemy there already
-        //     if (spaceTaken[row][column] == false) {
-        //         // first flip the switch
-        //         spaceTaken[row][column] = true;
-        //         // then spawn the enemy
-        //         enemy = this.add.sprite( 64 * column, 64 * row, 'turret');
-        //         enemy.enableBody = true;
+            // I now have coordinates for where I want the enemy to be placed
+            // next is to check whether there exists an enemy there already
+            if (spaceTaken[row][column] == false) {
+                // first flip the switch
+                spaceTaken[row][column] = true;
+                // then spawn the enemy
+                enemy = this.add.sprite( 64 * column, 64 * row, 'turret');
+                enemy.enableBody = true;
                 
-        //         // set their stats
-        //         enemy.health = 10;
-        //         enemy.fireRate = 3; // seconds
-        //         enemy.damage = 1;   // takes away 1 player heart
-        //         enemy.attackDirection = direction;
-        //         // . . .
+                // set their stats
+                enemy.health = 10;
+                enemy.fireRate = 3; // seconds
+                enemy.damage = 1;   // takes away 1 player heart
+                enemy.attackDirection = direction;
+                // . . .
                 
-        //         // console.log(enemy.attackDirection);
+                console.log(enemy.attackDirection);
                 
-        //         // then add them to the current list of enemies
-        //         enemies.push(enemy);
-        //     }
-
-        
-        //                                                       //
-        //                                                       //
-        //            END OF TURRET COMMENTED CODE               //
-        //                                                       //
-        //                                                       //
-
+                // then add them to the current list of enemies
+                enemies.push(enemy);
+            }
         }
     },
     
@@ -348,8 +301,7 @@ Starward.Game.prototype = {
         if (chestSpawned == true){
             this.physics.arcade.overlap(chest, player, this.collideMoney, null, this);
         }
-
-
+        
         // set the game over screen when player loses all of their lives
         if (currentLives == 0){
             this.state.start('GameOver');
@@ -426,62 +378,30 @@ Starward.Game.prototype = {
         // Player movement
         
         if (cursors.left.isDown) {
+            //  Move to the left
+            this.movePlayer(-1, 0);
             //player.body.velocity.x = -200;
             player.animations.play('walk_l');
-            if(player.x == 192){
-                return
-            }
-            else{
-                //  Move to the left
-                this.movePlayer(-1, 0);
-            }
-            
         }
         else if (cursors.right.isDown) {
             //  Move to the right
-            player.animations.play('walk_r');
-            if(player.x == 512){
-                return
-            }
-            else{
-                this.movePlayer(1,0);
-            }
+            this.movePlayer(1, 0);
             //player.body.velocity.x = 200;
+            player.animations.play('walk_r');
         }
         else if (cursors.up.isDown) {
+            this.movePlayer(0, -1);
             //player.body.velocity.y = -200;
             player.animations.play('walk_b');
-            if(player.y == 192){
-                return
-            }
-            else{
-                this.movePlayer(0,-1);
-            }
         }
         else if (cursors.down.isDown) {
+            this.movePlayer(0, 1);
             //player.body.velocity.y = 200;
             player.animations.play('walk_f');
-            if(player.y == 512){
-                return
-            }
-            else{
-                this.movePlayer(0,1);
-            }
         }
         else {
             player.animations.play('idle_f');
         }
-
-        
-        // When all enemies are defeated, spawn the chest to proceed to the shop
-//        if (numEnemies == 0 && chestSpawned == false){
-//            chest = this.add.sprite(128, 0, 'chest');
-//            chest.enableBody = true;
-//            this.physics.arcade.enable(chest);
-//            chestSpawned = true;
-//            this.proceedText = this.add.text(0, 64, 'Collect chest to proceed!', {font: '12px Comic Sans MS', fill: '#fff'});
-//        }
-
 
         if(this.gameover == true){
                 this.state.start('Shop');
